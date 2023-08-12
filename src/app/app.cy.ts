cb67bb8e43ccfef7600341e818c8b4a14c55b8e3
LAPTOP-P7G9LM4M\wumen <332982129@qq.com>
2023-08-12 20:28:02 +0800

    add: init cypress

---
+++ src/app/app.cy.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: LAPTOP-P7G9LM4M\wumen 332982129@qq.com
+ * @Date: 2023-08-12 10:11:25
+ * @LastEditors: LAPTOP-P7G9LM4M\wumen 332982129@qq.com
+ * @LastEditTime: 2023-08-12 10:12:43
+ * @FilePath: \chaofun-frontc:\Users\wumen\Documents\msdocs-nodejs-mongodb-azure-sample-app\project\src\app\app.cy.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true);
+  });
+});
